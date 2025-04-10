ae82459dbcb5c6ab8a353fbc3cd394b2839fe009
Ha-an1 <147293508+Ha-an1@users.noreply.github.com>
2025-04-10 18:40:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Print a message indicating the script has started
 echo "Running alert.py application"
 
-# Run the Python script
 python alert.py
 
-# Print a message indicating the script has finished
 echo "alert.py application executed successfully"
